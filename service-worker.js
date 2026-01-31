bdda45f942c6c049690ef8ed5cdb31788cfbacc8
iamtanasa <tandrei237@gmail.com>
2026-01-31 15:06:05 +0200

    app tel

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'univers-2222-v1';
+
+// Adaugă aici paginile principale și resursele esențiale
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/2222.html',
+  '/style.css',
+  '/script.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
